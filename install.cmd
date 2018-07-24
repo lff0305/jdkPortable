e897212b7bf759693b0589c9015583fb74a35155
Liu <lff0305@gmail.com>
2018-07-24 10:41:12 +0800

    No issue. Update blanks

--- install.cmd
+++ install.cmd
@@ -3,14 +3,14 @@
 echo 'Extracting .exe file...'
 set found="0"
 if exist jdk-8u181-windows-x64.exe (
-set found="1"
-echo found jdk-8u171-windows-x64.exe
-7z x jdk-8u181-windows-x64.exe
+	set found="1"
+	echo found jdk-8u171-windows-x64.exe
+	7z x jdk-8u181-windows-x64.exe
 )
 if exist jdk-8u171-windows-x64.exe (
-set found="1"
-echo found jdk-8u171-windows-x64.exe
-7z x jdk-8u171-windows-x64.exe
+	set found="1"
+	echo found jdk-8u171-windows-x64.exe
+	7z x jdk-8u171-windows-x64.exe
 )
 if %found% == "0" (
     echo JDK Installer not found. Exit.
